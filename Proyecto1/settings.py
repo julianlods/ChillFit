8f3cdcf407a34f7b02c39525bfe07ebac3f1da59
Julian Lods <julian.lods@gmail.com>
2025-03-31 22:47:04 -0300

    Config: soporte de email vía SMTP con variables de entorno

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -119,7 +119,13 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Configuración del servidor de correo para desarrollo
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 MEDIA_URL = '/media/'

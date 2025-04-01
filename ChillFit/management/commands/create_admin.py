5f426e18aa7956064140b5356e776d1ef3ad44b6
Julian Lods <julian.lods@gmail.com>
2025-04-01 10:52:17 -0300

    Agregar script create_admin.py para crear superusuario automáticamente

--- ChillFit/management/commands/create_admin.py
+++ ChillFit/management/commands/create_admin.py
@@ -2,12 +2,12 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Crea el usuario admin j'
+    help = 'Crea el usuario admin a'
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        if not User.objects.filter(username='j').exists():
-            User.objects.create_superuser('j', 'julian.lods@gmail.com', 'j')
-            self.stdout.write(self.style.SUCCESS('✔️ Superusuario j creado.'))
+        if not User.objects.filter(username='a').exists():
+            User.objects.create_superuser('a', 'a@correo.com', 'a')
+            self.stdout.write(self.style.SUCCESS('✔️ Superusuario "a" creado.'))
         else:
-            self.stdout.write('ℹ️ El usuario j ya existe.')
+            self.stdout.write('ℹ️ El usuario "a" ya existe.')

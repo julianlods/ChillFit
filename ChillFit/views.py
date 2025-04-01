53edae0ec730395079d25c4af8f0e1e9c83184f2
Julian Lods <julian.lods@gmail.com>
2025-03-31 21:54:18 -0300

    Fix: importar correctamente 'now' desde django.utils.timezone

--- ChillFit/views.py
+++ ChillFit/views.py
@@ -11,6 +11,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.utils import timezone
+from django.utils.timezone import now
 from django.conf import settings
 
 from .models import UsuarioRutina, Rutina, PerfilUsuario, PlanDeTrabajo, Pago, BloqueEjercicio
